2a71ca1d5c5dff6b52aff3d435f3af56b3d84929
Daniel Guzman <42854222+dannyguzman31@users.noreply.github.com>
2021-09-30 16:05:12 -0600

    updated Linked_list.js

--- Data_Structures/linked_list.js
+++ Data_Structures/linked_list.js
@@ -7,6 +7,12 @@
 // connected via nodes with a next pointer
 // random access is not allowed
 
+// -- BIG O /
+// Insertion: O(1)
+// Removal: Depends, O(1) or O(n)
+// Searching: O(n)
+// Access: O(n)
+
 // Class Node:
 // Stores Data (val) and references to next node (next)
 class Node {
@@ -75,44 +81,41 @@ class SinglyLinkedList {
       newNode.next = this.head;
       this.head = newNode;
     }
-      this.length++;
-      return this;
+    this.length++;
+    return this;
   }
 
-  // Get: Retrieves a node by it's position in the linked List 
+  // Get: Retrieves a node by it's position in the linked List
 
-  get(index){
-      if(index < 0 || index >= this.length) return null;
-      var counter = 0;
-      var current = this.head;
-      while(counter !== index){
-          current = current.next;
-          counter++;
-      }
-      return current;
-
+  get(index) {
+    if (index < 0 || index >= this.length) return null;
+    var counter = 0;
+    var current = this.head;
+    while (counter !== index) {
+      current = current.next;
+      counter++;
+    }
+    return current;
   }
 
   // Set: changes the value of a node based on it's position in the Linked list
 
-  set(index, val){
+  set(index, val) {
+    var foundNode = this.get(index);
 
-      var foundNode = this.get(index);
-
-      if(foundNode){
-        foundNode.val = val;
-        return true;
-      }
-      return false;
-
+    if (foundNode) {
+      foundNode.val = val;
+      return true;
+    }
+    return false;
   }
 
   // Insert: Addubg a node to the Linked list at a specific position
-  insert(index, val){
-    if(index < 0 || index > this.length) return false;
-    if(index === this.length) return !!this.push(val);
-    if(index === 0) return !!this.unshift(val);
-    
+  insert(index, val) {
+    if (index < 0 || index > this.length) return false;
+    if (index === this.length) return !!this.push(val);
+    if (index === 0) return !!this.unshift(val);
+
     var newNode = new Node(val);
     var prev = this.get(index - 1);
     var temp = prev.next;
@@ -120,22 +123,66 @@ class SinglyLinkedList {
     newNode.next = temp;
     this.length++;
     return true;
+  }
+
+  // Remove: removes a node from linked list
+  remove(index) {
+    if (index < 0 || index >= this.length) return undefined;
+    if (index === 0) return this.shift();
+    if (index === this.length - 1) return this.pop();
+    var previousNode = this.get(index - 1);
+    var removed = previousNode.next;
+    previousNode.next = removed.next;
+    this.length--;
+    return removed;
+  }
+
+
+  // Reverse: 
+
+  reverse(){
+      // switch head and tail 
+      var node = this.head;
+      this.head = this.tail;
+      this.tail = node;
+      // variables
+      var next;
+      var prev = null;
+      // loop through the linked list 
+      for ( var  i = 0; i < this.length; i++){
+        next = node.next;
+        node.next = prev;
+        prev = node;
+        node = next;
+      }
+      return this;
+  }
+// prints in an array
+  print(){
+    var arr = [];
+    var current = this.head
+    while(current){
+        arr.push(current.val)
+        current = current.next
+    }
+    console.log(arr);
 }
-
-// Remove: removes a node from linked list
-
 
 }
 
 var list = new SinglyLinkedList();
-list.push("test1");
-list.push("test2");
-list.push("test3");
-list.push("test4");
-list.push("test5");
-list.push("test6");
-list.pop();
+list.push(100);
+list.push(201);
+list.push(250);
+list.push(350);
+list.push(355);
+list.push(400);
+list.push(425);
+list.push(550);
+list.push(555);
+list.push(999);
 
-console.log(list);
 
-console.log(list.unshift("test1"));
+console.log(list.print());
+
+console.log(list.reverse());
