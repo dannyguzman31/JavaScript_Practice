e936c7c39c38b6c78526d0181dbd6aebe7e802a9
Daniel Guzman <42854222+dannyguzman31@users.noreply.github.com>
2021-10-08 16:13:06 -0600

    Create graph.js

---
+++ Data_Structures/graph.js
@@ -0,0 +1,13 @@
+/***************** GRAPHS  *********************/
+
+// A Graph is a non-linear data structure consisting of nodes and edges. The nodes are sometimes also referred to as vertices and the edges are lines or arcs that connect any two nodes in the graph.
+
+/* Adjancency List           VS              Adjancency Matrix  
+
+- Can take up less space                     
+- Faster to iterate over 
+  all edges 
+                                        - Faster to lookup specific edge
+
+*/
+
