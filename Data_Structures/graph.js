11268dcd25df9891b8e57ff12c1ce9212403898c
Daniel Guzman <42854222+dannyguzman31@users.noreply.github.com>
2021-10-11 10:09:29 -0600

    Update graph.js

--- Data_Structures/graph.js
+++ Data_Structures/graph.js
@@ -10,4 +10,41 @@
                                         - Faster to lookup specific edge
 
 */
+class Graph{
+  constructor(){
+      this.adjacencyList = {};
+  }
 
+  // addVertex: accepts a vertex, adds a vertex to the adjacency list 
+  addVertex(vertex){
+      if(!this.adjacencyList[vertex]) this.adjacencyList[vertex] = [];
+  }
+
+  // AddEdge: accepts two values, finds the adjacency list key of v1 and v2 and push to the array 
+
+  addEdge(v1,v2){
+    this.adjacencyList[v1].push(v2);
+    this.adjacencyList[v2].push(v1);
+}
+  // RemoveEdge: accepts two values, reassigns the key of v1 and v2 to be an array that does not contain v1 & v2
+removeEdge(vertex1,vertex2){
+  this.adjacencyList[vertex1] = this.adjacencyList[vertex1].filter(
+      v => v !== vertex2
+  );
+  this.adjacencyList[vertex2] = this.adjacencyList[vertex2].filter(
+      v => v !== vertex1
+  );
+}
+
+  // RemoveVertex: accepts a vertex to remove, loops as long as there any other vertices in the adjacency list of that vertex. calls the removeEdge to remove the values. 
+removeVertex(vertex){
+  while(this.adjacencyList[vertex].length){
+      const adjacentVertex = this.adjacencyList[vertex].pop();
+      this.removeEdge(vertex, adjacentVertex);
+  }
+  delete this.adjacencyList[vertex]
+}
+
+
+  // end 
+}
