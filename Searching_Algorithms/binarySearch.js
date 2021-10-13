d37b2284d42bc10fe5eccb4efd1fc986865875df
Daniel Guzman <42854222+dannyguzman31@users.noreply.github.com>
2021-10-13 14:51:42 -0600

    graphTraversal

--- Searching_Algorithms/binarySearch.js
+++ Searching_Algorithms/binarySearch.js
@@ -2,25 +2,25 @@
 // splits sorted arrays/lists/strings in halfs, eliminating whats not used. 
 
 
-function binarySearch(array, value){
+function binarySearch(nums, target){
 
-    // create a left pointer at the start of the array 
+    // create a left pointer at the start of the nums 
     let leftPointer = 0;
-    // create a right pointer at the end of the array 
-    let rightPointer = array.length -1;
+    // create a right pointer at the end of the nums 
+    let rightPointer = nums.length -1;
     // create a pointer in the middle 
     let middle = Math.floor((leftPointer + rightPointer) / 2);
 
     // while the left pointer comes before the right pointer 
-    while( array[middle] != value && leftPointer <= rightPointer){
+    while( nums[middle] != target && leftPointer <= rightPointer){
 
-        if(value < array[middle]) rightPointer = middle - 1;
+        if(target < nums[middle]) rightPointer = middle - 1;
         else leftPointer = middle + 1;
         middle = Math.floor((leftPointer + rightPointer) / 2);
     }
 
-    // never find the value, return -1
-    return array[middle] === value ? middle : -1;
+    // never find the target, return -1
+    return nums[middle] === target ? middle : -1;
 }
 
 console.log(binarySearch([2,5,6,9,13,15,21,85,98,110,152,333], 98));
